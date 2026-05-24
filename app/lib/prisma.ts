98725886fd9d4829a93ff76bb697a8fb282bf2b5
Prasad <prasad.project2025@gmail.com>
2026-05-24 23:49:52 +0530

    Fix Prisma import

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,21 +1,15 @@
-import { PrismaClient } from '../app/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client'
 
 declare global {
-  // allow global prisma across hot-reloads in development
-  // eslint-disable-next-line vars-on-top
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined
 }
 
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error('Missing DATABASE_URL environment variable');
+export const prisma =
+  global.prisma ||
+  new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma
 }
 
-export const prisma = global.prisma || new PrismaClient({
-  adapter: new PrismaPg({ connectionString: databaseUrl }),
-});
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
-
-export default prisma;
+export default prisma
